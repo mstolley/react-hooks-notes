ed63607542e863a85e57ec234aafdb67060c11af
Matthew Stolley <matthew.stolley@gmail.com>
2019-04-12 16:58:24 -0600

    Format time better.

--- src/components/Time.js
+++ src/components/Time.js
@@ -5,7 +5,7 @@ const Time = ({type, date}) => {
   if(type === 'date') {
     date = new Date(date).toDateString();
   } else if(type === 'time') {
-    date = new Date(date).toLocaleTimeString();
+    date = new Date(date).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true});
   } else {
     date = new Date(date).toLocaleString();
   }
